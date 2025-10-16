b352d3f4039763e0cde73cd879e1135857c47221
dmiroshnichenko02 <danil.miroshnichenko2022@gmail.com>
2025-10-16 22:23:41 +0300

    discussions and venue, set acf fields on admin panel

--- templates/flex-discussions.php
+++ templates/flex-discussions.php
@@ -8,28 +8,38 @@ $subtitle = $args['subtitle'] ?? '';
 $content_repeater = $args['content_repeater'] ?? [];
 $button = $args['button'] ?? [];
 ?>
-<section class="relative py-16 mb-16 flex flex-col gap-8" style="background-image:url('<?= esc_url($background) ?>');background-size:cover;background-position:center;">
-    <div class="container mx-auto px-6 md:px-16">
-        <h2 class="text-3xl font-bold mb-4"><?= esc_html($title) ?></h2>
-        <div class="mb-6 text-xl text-gray-600"><?= esc_html($subtitle) ?></div>
+<section class="relative pb-[97px]" >
+    <div class="pb-[46px]" style="background-image:url('<?= esc_url($background) ?>');background-size:cover;background-position:center;">
+        <div class="pl-[calc((100%-1296px)/2)] flex items-end min-h-[700px] justify-between">
+            <h2 class="text-h2 leading-h2 font-archivo font-light uppercase text-white max-w-[600px]"><?= esc_html($title) ?></h2>
+            <div class="mb-6 text-xl text-gray-600 pb-[10px] border-b border-t border-solid border-white max-w-[448px]">
+                <div class="pr-[75px] py-[30px] uppercase text-white font-archivo font-light text-h6 leading-h6">
+                    <?= esc_html($subtitle) ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container mx-auto">
+        
         <?php if(is_array($content_repeater)): ?>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-8 mb-8">
+            <div class="pl-[42px] relative flex flex-col gap-4 mb-[51px]">
+                <div class="absolute left-3 top-0 w-[1px] h-[96%] bg-black-20"></div>
                 <?php foreach($content_repeater as $item): ?>
-                    <div class="bg-white rounded shadow p-4">
-                        <div class="font-semibold text-lg mb-2"><?= esc_html($item['label']) ?></div>
-                        <div class="font-bold text-xl mb-2"><?= esc_html($item['title']) ?></div>
-                        <div class="prose prose-sm max-w-none"><?= wp_kses_post($item['text']) ?></div>
+                    <div class="mt-[60px] flex flex-col gap-2 relative">
+                        <div class="absolute top-[45%] left-[-42px] -translate-y-1/2 w-6 h-6 bg-black rounded-full"></div>
+
+                        <div class="font-archivo text-label leading-label text-black-60 font-light uppercase"><?= esc_html($item['label']) ?></div>
+                        <div class="font-archivo text-h4 leading-h4 text-black font-light uppercase"><?= esc_html($item['title']) ?></div>
+                        <div class="font-archivo text-medium leading-medium text-black font-light"><?= wp_kses_post($item['text']) ?></div>
                     </div>
                 <?php endforeach; ?>
             </div>
         <?php endif; ?>
 
         <?php if (!empty($button['title']) && !empty($button['link'])): ?>
-            <div>
-                <a class="inline-block bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-800 transition" href="<?= esc_url($button['link']) ?>">
+                <a class="bg-[linear-gradient(90deg,_#0065CA_0%,_#37B6FF_100%)] inline-block text-white font-archivo text-medium leading-medium px-[53px] py-[22px] transition" href="<?= esc_url($button['link']) ?>">
                     <?= esc_html($button['title']) ?>
                 </a>
-            </div>
         <?php endif; ?>
     </div>
 </section>
--- templates/flex-venue.php
+++ templates/flex-venue.php
@@ -7,24 +7,83 @@ $subtitle = $args['subtitle'] ?? '';
 $location = $args['location'] ?? '';
 $logotype = $args['logotype'] ?? '';
 $images = $args['images'] ?? [];
+$background = $args['background'] ?? '';
 ?>
-<section class="mb-16 py-16 bg-gray-50">
-    <div class="container mx-auto px-6 md:px-16 flex flex-wrap gap-12">
-        <div class="w-full md:w-1/2 flex flex-col gap-6">
-            <h2 class="text-3xl font-bold mb-2"><?= esc_html($title) ?></h2>
-            <div class="mb-2 text-gray-500 text-lg"><?= esc_html($location) ?></div>
-            <div class="prose prose-sm max-w-none mb-4"><?= wp_kses_post($subtitle) ?></div>
+<section class="h-[1072px] py-[90px] overflow-hidden" style="background-image:url('<?= esc_url($background) ?>');background-size:cover;background-position:center;">
+    <div class="container mx-auto">
+        <div class="w-full flex justify-between">
+           <div class="max-w-[577px]">
+                <h2 class="text-h2 leading-h2 font-archivo font-light uppercase text-white mb-3"><?= esc_html($title) ?></h2>
+                <div class="font-archivo text-medium leading-medium text-white font-light mb-[30px]"><?= wp_kses_post($subtitle) ?></div>
+                <div class="flex gap-3 items-center font-archivo text-medium leading-medium text-white font-light">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M9 11C9 11.7956 9.31607 12.5587 9.87868 13.1213C10.4413 13.6839 11.2044 14 12 14C12.7956 14 13.5587 13.6839 14.1213 13.1213C14.6839 12.5587 15 11.7956 15 11C15 10.2044 14.6839 9.44129 14.1213 8.87868C13.5587 8.31607 12.7956 8 12 8C11.2044 8 10.4413 8.31607 9.87868 8.87868C9.31607 9.44129 9 10.2044 9 11Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M17.657 16.6567L13.414 20.8997C13.039 21.2743 12.5306 21.4848 12.0005 21.4848C11.4704 21.4848 10.962 21.2743 10.587 20.8997L6.343 16.6567C5.22422 15.5379 4.46234 14.1124 4.15369 12.5606C3.84504 11.0087 4.00349 9.40022 4.60901 7.93844C5.21452 6.47665 6.2399 5.22725 7.55548 4.34821C8.87107 3.46918 10.4178 3 12 3C13.5822 3 15.1289 3.46918 16.4445 4.34821C17.7601 5.22725 18.7855 6.47665 19.391 7.93844C19.9965 9.40022 20.155 11.0087 19.8463 12.5606C19.5377 14.1124 18.7758 15.5379 17.657 16.6567Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                    <span><?= esc_html($location) ?></div></span>
+           </div>
             <?php if ($logotype): ?>
-                <img src="<?= esc_url($logotype) ?>" alt="Venue logo" class="h-20 object-contain" />
+                <img src="<?= esc_url($logotype) ?>" alt="Venue logo" class="h-[105px] w-[240px] object-cover" />
             <?php endif; ?>
         </div>
-        <?php if (is_array($images) && count($images) > 0): ?>
-        <div class="w-full md:w-1/2 grid grid-cols-2 gap-4 place-items-center">
-            <?php foreach ($images as $img):
-                $src = is_array($img['image']) ? $img['image']['url'] ?? '' : $img['image']; ?>
-                <?php if ($src): ?><img src="<?= esc_url($src) ?>" alt="Venue gallery image" class="object-cover rounded-xl shadow-lg" /><?php endif; ?>
-            <?php endforeach; ?>
-        </div>
-        <?php endif; ?>
     </div>
+    <?php if (is_array($images) && count($images) > 0): ?>
+        <div class="pl-[calc((100%-1296px)/2)] relative mt-[45px]">
+            <div class="swiper venue-swiper">
+                <div class="swiper-wrapper">
+                    <?php foreach ($images as $img):
+                        $src = is_array($img['image']) ? $img['image']['url'] ?? '' : $img['image']; ?>
+                        <?php if ($src): ?>
+                            <div class="swiper-slide">
+                                <img src="<?= esc_url($src) ?>" alt="Venue gallery image" class="object-cover h-[564px] w-full" style="max-width:unset;"/>
+                            </div>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+            <div class="absolute bottom-[-104px] right-[calc((100%-1296px)/2)] flex gap-4 z-20">
+                    <button class="venue-swiper-button-prev w-[63px] h-[63px] flex items-center justify-center rounded-full cursor-pointer bg-[#F5F5F5] hover:bg-[#BCBCBC]" type="button">
+                    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clip-path="url(#clip0_322_1022)">
+                        <path d="M13.5 7L0.499999 7M0.499999 7L6.5 1M0.499999 7L6.5 13" stroke="#333333" stroke-linecap="round" stroke-linejoin="round"/>
+                        </g>
+                        <defs>
+                        <clipPath id="clip0_322_1022">
+                        <rect width="14" height="14" fill="white" transform="translate(0 14) rotate(-90)"/>
+                        </clipPath>
+                        </defs>
+                    </svg>
+                    </button>
+                    <button class="venue-swiper-button-next w-[63px] h-[63px] flex items-center justify-center rounded-full cursor-pointer bg-[#F5F5F5] hover:bg-[#BCBCBC]" type="button">
+                    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clip-path="url(#clip0_322_1020)">
+                        <path d="M0.500001 7L13.5 7M13.5 7L7.5 13M13.5 7L7.5 1" stroke="#333333" stroke-linecap="round" stroke-linejoin="round"/>
+                        </g>
+                        <defs>
+                        <clipPath id="clip0_322_1020">
+                        <rect width="14" height="14" fill="white" transform="translate(14) rotate(90)"/>
+                        </clipPath>
+                        </defs>
+                    </svg>
+                    </button>
+                </div>
+        </div>
+        <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            new Swiper('.venue-swiper', {
+                slidesPerView: 2,
+                loop: true,
+                spaceBetween: 24,
+                navigation: {
+                    nextEl: '.venue-swiper-button-next',
+                    prevEl: '.venue-swiper-button-prev',
+                },
+                breakpoints: {
+                    320: { slidesPerView: 1 },
+                    900: { slidesPerView: 2 }
+                }
+            });
+        });
+        </script>
+    <?php endif; ?>
 </section>
